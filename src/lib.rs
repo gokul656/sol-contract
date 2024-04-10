aa9900327e70f1743d5c39d9507bb5dcd67d58e2
gokul656 <kgokul656@gmail.com>
2024-04-10 16:01:21 +0530

    added instruction enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,37 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+    account_info::{next_account_info, AccountInfo}, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    program_id: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     msg!("contract_instantiated!");
-    msg!("program_id {}", program_id);
-    msg!("account_info {:?}", accounts);
-    msg!("instruction_data {:?}", instruction_data);
 
-    msg!("hello world!");
+    let accounts_iter = &mut accounts.iter();
+    let caller_account = next_account_info(accounts_iter)?;
+
+    msg!("incoming request from: {:?}", caller_account.key);
+    
+    let deserialized_data =
+        Instruction::try_from_slice(instruction_data).expect("unable to desrialize instruction");
+
+    msg!(
+        "initiating destruction sequence for target {}",
+        deserialized_data.payload
+    );
 
     Ok(())
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Instruction {
+    pub payload: String,
+    pub created_at: u64,
 }
 
 #[cfg(test)]
@@ -28,7 +43,7 @@ mod test {
 
     #[test]
     pub fn test_instantiated() {
-        let key = Pubkey::default();
+        let key = Pubkey::new_unique();
         let owner = Pubkey::default();
         let mut lamports = 0;
         let mut data = vec![0; mem::size_of::<u32>()];
@@ -46,8 +61,16 @@ mod test {
         );
 
         let accounts = vec![account];
-        let instruction_data = Vec::new();
+        let instruction_data = Instruction {
+            payload: "12.9716° N, 77.5946° E".to_string(),
+            created_at: 32,
+        };
 
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap()
+        let mut serialzed_instruction = vec![];
+        instruction_data
+            .serialize(&mut serialzed_instruction)
+            .unwrap();
+
+        process_instruction(&program_id, &accounts, &serialzed_instruction).unwrap()
     }
 }
